82c9d6434ea930fd37010b85d70a8ea93f9942d2
Rashad786 <mohdrashad522@gmail.com>
2025-11-04 11:25:26 +0530

    Time: 0 ms (100%), Space: 41.9 MB (5.91%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Memoization
+
+    // public int solve(int n, int[] dp) {
+    //     if(n==0 || n==1) return n;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    // }
+    public int fib(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[n+1];
+
+        // Arrays.fill(dp, -1);
+        // return solve(n, dp);
+
+        // Tabulation
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
