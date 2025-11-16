3fb80c33d459e0e63472abfae1fdc31ca5830fca
Rashad786 <mohdrashad522@gmail.com>
2025-11-16 14:20:28 +0530

    Time: 5 ms (57.07%), Space: 46.3 MB (14.66%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -1,21 +1,14 @@
 class Solution {
     public int numSub(String s) {
         int n = s.length();
-        int[] prefix = new int[n];
         int mod = 1000000007;
-
-        if(s.charAt(0)=='1') prefix[0] = 1;
-        for(int i=1;i<n;i++) {
-            if(s.charAt(i)=='1')
-                prefix[i]  = 1 + prefix[i-1];
-        }
 
         int i=0;
         int j=0;
         int ans=0;
         while(j<n) {
-            if(prefix[j]==0) {
-                while(j<n && prefix[j]==0)j++;
+            if(s.charAt(j)=='0') {
+                while(j<n && s.charAt(j)=='0') j++;
                 i=j;
             }else{
                 ans = (ans + j-i+1) % mod;
