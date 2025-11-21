2577e6f5a51d3f9c0b941e00b920d1c3455a03da
Rashad786 <mohdrashad522@gmail.com>
2025-11-21 11:46:58 +0530

    Time: 195 ms (55.01%), Space: 46.8 MB (28.57%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,21 +1,24 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
         int n=s.length();
-        Set<Character> letters = new HashSet<>();
+        int[][] hash = new int[26][2];
+        for(int[]row: hash) Arrays.fill(row, -1);
 
-        for(char letter: s.toCharArray()) letters.add(letter);
+        for(int i=0;i<n;i++) {
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+            if(hash[idx][0]==-1) hash[idx][0] = i;
+            hash[idx][1] = i;
+        }
 
         int cnt=0;
-        for(char letter: letters) {
-            int leftIdx = -1;
-            int rightIdx = -1;
+        for(int i=0;i<26;i++) {
 
-            for(int i=0;i<n;i++) {
-                if(s.charAt(i) == letter) {
-                    if(leftIdx==-1) leftIdx = i;
-                    rightIdx = i;
-                }
-            }
+
+            int leftIdx = hash[i][0];
+            int rightIdx = hash[i][1];
+
+            if(leftIdx==-1) continue;
 
             Set<Character> st = new HashSet<>();
             for(int mid=leftIdx+1;mid<rightIdx;mid++) {
