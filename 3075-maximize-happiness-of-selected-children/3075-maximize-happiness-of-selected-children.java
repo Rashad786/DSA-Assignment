dd5c84e3aafc3f07a50d319e300b575c857ad8d4
Rashad786 <mohdrashad522@gmail.com>
2025-12-25 15:09:18 +0530

    Time: 45 ms (22.58%), Space: 109.1 MB (65.05%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        long ans=0;
+        int n=happiness.length;
+        Arrays.sort(happiness);
+        int dec=0;
+        for(int i=n-1;i>=0 && k>0;i--) {
+            int temp = happiness[i]-dec;
+            if(temp>0) ans+=temp;
+            dec++; k--;
+        }
+        return ans;
+    }
+}
