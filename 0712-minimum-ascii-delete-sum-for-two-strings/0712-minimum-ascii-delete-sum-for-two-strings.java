e670735d89d0b78bff698ce0a5a0e231923bf52e
Rashad786 <mohdrashad522@gmail.com>
2026-01-10 11:47:55 +0530

    Time: 21 ms (57.82%), Space: 46.9 MB (13.26%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -26,9 +26,31 @@ class Solution {
         int m = s1.length();
         int n = s2.length();
         dp = new int[m+1][n+1];
-        for(int[]row: dp) Arrays.fill(row, -1);
-        return solve(m, n, s1, s2);
+        // for(int[]row: dp) Arrays.fill(row, -1);
+        // return solve(m, n, s1, s2);
 
-        
+        for(int j=0;j<=n;j++) {
+            int temp=0;
+            for(int k=j;k>0;k--) temp += (int)s2.charAt(k-1);
+            dp[0][j] = temp;
+        }
+        for(int i=0;i<=m;i++) {
+            int temp=0;
+            for(int k=i;k>0;k--) temp += (int)s1.charAt(k-1);
+            dp[i][0] = temp;
+        }
+
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else {
+                    int x = (int)s1.charAt(i-1) + dp[i-1][j];
+                    int y = (int)s2.charAt(j-1) + dp[i][j-1];
+
+                    dp[i][j] = Math.min(x, y);
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
